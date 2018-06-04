6e809dbc82bdaf5053772053d10ff1a067dca306
Josh Singer <josh.singer@glasgow.ac.uk>
2018-06-04 17:59:50 +0100

    various updates.

--- modules/ncbiRabvUpdateCuratedSequences.js
+++ modules/ncbiRabvUpdateCuratedSequences.js
@@ -28,3 +28,120 @@ function pad(num, size) {
     return s;
 }
 
+
+function placeCuratedAll() {
+	glue.log("INFO", "Deleting files in placement path "+placement.path);
+	var placementPathFiles = glue.command(["file-util", "list-files", "--directory", placement.path], {convertTableToObjects:true});
+	_.each(placementPathFiles, function(placementPathFile) {
+		glue.command(["file-util", "delete-file", placement.path+"/"+placementPathFile.fileName]);
+	});
+	glue.log("INFO", "Deleted "+placementPathFiles.length+" files");
+	var fileSuffix = 1;
+	var whereClause = "source.name = 'ncbi-curated'";
+	placeCurated(whereClause, fileSuffix);
+}
+
+function placeCuratedIncoming() {
+	var placementPathFiles = glue.command(["file-util", "list-files", "--directory", placement.path], {convertTableToObjects:true});
+	var placementFileNames = _.map(placementPathFiles, function(placementPathFile) {return placementPathFile.fileName;});
+	var fileSuffix = 1;
+	while(true) {
+		fileSuffixString = pad(fileSuffix, 6);
+		if(_.contains(placementFileNames, placement.prefix + fileSuffixString + ".xml")) {
+			fileSuffix++;
+		} else {
+			break;
+		}
+	}
+	var whereClause = "source.name = 'ncbi-curated' and ncbi_incoming = true";
+	placeCurated(whereClause, fileSuffix);
+}
+
+function placeCurated(whereClause, fileSuffix) {
+	glue.log("INFO", "Counting sequences where "+whereClause);
+	var numSequences = glue.command(["count", "sequence", "--whereClause", whereClause]).countResult.count;
+	glue.log("INFO", "Found "+numSequences+" sequences where "+whereClause);
+	var batchSize = 50;
+	var offset = 0;
+	while(offset < numSequences) {
+		glue.log("INFO", "Placing sequences starting at offset "+offset);
+		glue.inMode("module/"+modules.placer, function() {
+			fileSuffixString = pad(fileSuffix, 6);
+			var outputFile = placement.path + "/" + placement.prefix + fileSuffixString + ".xml";
+			glue.command(["place", "sequence", 
+                           "--whereClause", whereClause,
+                           "--pageSize", batchSize, "--fetchLimit", batchSize, "--fetchOffset", offset, 
+                           "--outputFile", outputFile]);
+		});
+		offset += batchSize;
+		fileSuffix++;
+	}
+}
+
+
+function genotypeCurated() {
+	var placementPathFiles = glue.command(["file-util", "list-files", "--directory", placement.path], {convertTableToObjects:true});
+	_.each(placementPathFiles, function(placementPathFile) {
+		glue.log("INFO", "Computing genotype results for placement file "+placementPathFile.fileName);
+		var batchGenotyperResults;
+		glue.inMode("module/"+modules.genotyper, function() {
+			batchGenotyperResults = glue.command(
+					["genotype", "placer-result", 
+					 "--fileName", placement.path+"/"+placementPathFile.fileName, 
+					 "--detailLevel", "HIGH"], 
+					{convertTableToObjects:true});
+		});
+		glue.log("INFO", "Assigning genotype metadata for "+batchGenotyperResults.length+" genotyping results from placement file "+placementPathFile.fileName);
+		var batchSize = 500;
+		var numUpdates = 0;
+		_.each(batchGenotyperResults, function(genotyperResult) {
+			var queryBits = genotyperResult.queryName.split("/");
+			var sourceName = queryBits[0];
+			var sequenceID = queryBits[1];
+
+			if(genotyperResult.minor_cladeFinalClade != null) {
+				targetAlignmentName = genotyperResult.minor_cladeFinalClade;
+			} else if(genotyperResult.minor_cladeFinalClade != null) {
+				targetAlignmentName = genotyperResult.major_cladeFinalClade;
+			} else {
+				targetAlignmentName = "AL_MASTER";
+			}
+			
+			glue.inMode("alignment/"+targetAlignmentName, function() {
+				glue.command(["add", "member", sourceName, sequenceID]);
+			});
+			
+			glue.command(["compute", "alignment", targetAlignmentName, "rabvCompoundAligner",
+			              "--whereClause", "sequence.source.name = '"+sourceName+"' and sequence.sequenceID = '"+sequenceID+"'"]);
+
+			glue.inMode("sequence/"+sourceName+"/"+sequenceID, function() {
+					
+				var majorClade = genotyperResult.major_cladeFinalClade;
+				if(majorClade) {
+					var mjcRegex = /AL_([^_]+)/;
+					var mjcMatch = mjcRegex.exec(majorClade);
+					if(mjcMatch) {
+						glue.command(["set", "field", "--noCommit", "major_clade", mjcMatch[1]]);
+					}
+				}
+				var minorClade = genotyperResult.minor_cladeFinalClade;
+				if(minorClade) {
+					var mncRegex = /AL_[^_]+_([^_]+)/;
+					var mncMatch = mncRegex.exec(minorClade);
+					if(mncMatch) {
+						glue.command(["set", "field", "--noCommit", "minor_clade", mncMatch[1]]);
+					}
+				}
+			});
+			if(numUpdates % batchSize == 0) {
+				glue.command("commit");
+				glue.command("new-context");
+				glue.log("FINE", "Major/minor clade assigned for "+numUpdates+" sequences.");
+			}
+			numUpdates++;
+		});
+		glue.command("commit");
+		glue.command("new-context");
+		glue.log("FINE", "Major/minor clade assigned for "+numUpdates+" sequences.");
+	});
+}
