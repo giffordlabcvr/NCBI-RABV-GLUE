86e9e9a3b9b53043a9692ffbe045aaf68daf03d2
Robert Gifford <robjgiff@gmail.com>
2020-10-28 13:20:44 +0000

    Reformatting metadata, implementing import script (incomplete)

--- glue/ncbiRabvImportCuratedSideData.js
+++ glue/ncbiRabvImportCuratedSideData.js
@@ -11,24 +11,24 @@ for(var i = 0; i < infiles.length; i++) {
 		glue.log("INFO", "load result was:", loadResult);
 	});
 
-	_.each(loadResult, function(eveObj) {
+	_.each(loadResult, function(isolateObj) {
 
-		glue.inMode("sequence/curated/"+source_name+"/"+eveObj.sequenceID, function() {
+		glue.inMode("sequence/ncbi-curated/"+isolateObj.sequenceID, function() {
 	
-			glue.log("INFO", "Entering sequence table data for EVE reference 1:", eveObj.sequenceID);
+			glue.log("INFO", "Entering sequence table data for EVE reference 1:", isolateObj.sequenceID);
 
-			glue.command(["set", "field", "subfamily", eveObj.virus_subfamily]);
-			glue.command(["set", "field", "genus", eveObj.virus_genus]);
-			glue.command(["set", "field", "assign_clade", eveObj.assign_clade]);
-			glue.command(["set", "field", "assign_subclade", eveObj.assign_subclade]);
-			glue.command(["set", "field", "clade_ns", eveObj.virus_clade_ns]);
-			glue.command(["set", "field", "subclade_ns", eveObj.virus_subclade_ns]);
-			glue.command(["set", "field", "clade_vp", eveObj.virus_clade_vp]);
-			glue.command(["set", "field", "subclade_vp", eveObj.virus_subclade_vp]);
+			glue.command(["set", "field", "subfamily", isolateObj.virus_subfamily]);
+			glue.command(["set", "field", "genus", isolateObj.virus_genus]);
+			glue.command(["set", "field", "assign_clade", isolateObj.assign_clade]);
+			glue.command(["set", "field", "assign_subclade", isolateObj.assign_subclade]);
+			glue.command(["set", "field", "clade_ns", isolateObj.virus_clade_ns]);
+			glue.command(["set", "field", "subclade_ns", isolateObj.virus_subclade_ns]);
+			glue.command(["set", "field", "clade_vp", isolateObj.virus_clade_vp]);
+			glue.command(["set", "field", "subclade_vp", isolateObj.virus_subclade_vp]);
 
-			glue.log("INFO", "Entering remaining sequence table data for EVE reference:", eveObj.sequenceID);
-			glue.command(["set", "field", "name", eveObj.insertion_name]);
-			glue.command(["set", "field", "full_name", eveObj.insertion_full_name]);
+			glue.log("INFO", "Entering remaining sequence table data for EVE reference:", isolateObj.sequenceID);
+			glue.command(["set", "field", "name", isolateObj.insertion_name]);
+			glue.command(["set", "field", "full_name", isolateObj.insertion_full_name]);
 
 		});
 
