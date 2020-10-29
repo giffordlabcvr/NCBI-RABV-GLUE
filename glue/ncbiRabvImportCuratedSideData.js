1ae14ece1bb2a59c81c2b8bcae6a8edb7982b17f
Robert Gifford <robjgiff@gmail.com>
2020-10-29 12:55:43 +0000

    Updated script import logic

--- glue/ncbiRabvImportCuratedSideData.js
+++ glue/ncbiRabvImportCuratedSideData.js
@@ -1,3 +1,4 @@
+//var infiles = [ "Africa3miss.tsv" , "Africa2miss.tsv", "Arctic_miss.tsv" ];
 var infiles = [ "Africa3miss.tsv" ];
 
 // Get the sequence source mappings (i.e. which in ncbi-refseqs, ncbi-curated, etc)
@@ -26,8 +27,7 @@ for(var i = 0; i < infiles.length; i++) {
         	var countryEdit   = isolateObj.country_edit;
         	var countryUpdate = isolateObj.Country_update;
 		    //glue.log("INFO", "FIELD: countryEdit:", countryEdit);
-		    //glue.log("INFO", "FIELD: countryUpdate:", countryUpdate);
-		    
+		    //glue.log("INFO", "FIELD: countryUpdate:", countryUpdate);	    
 		    if (countryUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating country field:", isolateObj.country_edit);
 				//glue.command(["set", "field", "m49_country", isolateObj.country_edit]);				
@@ -37,7 +37,6 @@ for(var i = 0; i < infiles.length; i++) {
         	var placeUpdate = isolateObj.place_update;
  		    //glue.log("INFO", "FIELD: placeEdit:", placeEdit);
 		    //glue.log("INFO", "FIELD: placeUpdate:", placeUpdate);
-
 		    if (placeUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating place field:", isolateObj.placeEdit);
 				//glue.command(["set", "field", "m49_country", isolateObj.placeEdit]);				
@@ -47,7 +46,6 @@ for(var i = 0; i < infiles.length; i++) {
         	var collectionYearUpdate = isolateObj.Collection_year_update;
  		    //glue.log("INFO", "FIELD: collectionYearEdit:", collectionYearEdit);
 		    //glue.log("INFO", "FIELD: collectionYearUpdate:", collectionYearUpdate);
-
 		    if (collectionYearUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating collectionYearEdit field:", isolateObj.collectionYearEdit);
 				//glue.command(["set", "field", "collection_year", isolateObj.collectionYearEdit]);				
@@ -57,7 +55,6 @@ for(var i = 0; i < infiles.length; i++) {
         	var earliestCollectionYearUpdate = isolateObj.earliest_collection_update;
  		    //glue.log("INFO", "FIELD: earliestCollectionYearEdit:", earliestCollectionYearEdit);
 		    //glue.log("INFO", "FIELD: earliestCollectionYearUpdate:", earliestCollectionYearUpdate);
-
 		    if (earliestCollectionYearUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating earliest_collection_year field:", isolateObj.earliestCollectionYearEdit);
 				//glue.command(["set", "field", "earliest_collection_year", isolateObj.earliestCollectionYearEdit]);				
@@ -67,7 +64,6 @@ for(var i = 0; i < infiles.length; i++) {
         	var latestCollectionYearUpdate   = isolateObj.latest_collection_update;
  		    //glue.log("INFO", "FIELD: latestCollectionYearEdit:", latestCollectionYearEdit);
 		    //glue.log("INFO", "FIELD: latestCollectionYearUpdate:", latestCollectionYearUpdate);
-
 		    if (latestCollectionYearUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating latestCollectionYearEdit field:", isolateObj.latestCollectionYearEdit);
 				//glue.command(["set", "field", "latest_collection_year", isolateObj.latestCollectionYearEdit]);				
@@ -79,7 +75,6 @@ for(var i = 0; i < infiles.length; i++) {
  		    //glue.log("INFO", "FIELD: hostEdit:", hostEdit);
 		    //glue.log("INFO", "FIELD: hostUpdate:", hostUpdate);
 		    glue.log("INFO", "FIELD: hostCommonEdit:", hostCommonEdit);
-
 		    if (hostUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating host field:", isolateObj.hostEdit);
 				glue.log("INFO", "Updating host common name field:", isolateObj.hostCommonEdit);
@@ -88,8 +83,8 @@ for(var i = 0; i < infiles.length; i++) {
 
         	var labConstruct   = isolateObj.Lab_construct;
         	var vaccine        = isolateObj.vaccine;
- 		    //glue.log("INFO", "FIELD: labConstruct:", labConstruct);
-		    //glue.log("INFO", "FIELD: vaccine:", vaccine);
+ 		    glue.log("INFO", "FIELD: labConstruct:", labConstruct);
+		    glue.log("INFO", "FIELD: vaccine:", vaccine);
 			//glue.command(["set", "field", "lab_construct", isolateObj.labConstruct]);				
 			//glue.command(["set", "field", "vaccine_strain", isolateObj.vaccine]);				
 
@@ -101,7 +96,6 @@ for(var i = 0; i < infiles.length; i++) {
 				glue.log("INFO", "Updating gb_pubmed_id field:", isolateObj.pubmedEdit);
 				//glue.command(["set", "field", "host", isolateObj.hostEdit]);				
 		    }
-
 
 		});
 
