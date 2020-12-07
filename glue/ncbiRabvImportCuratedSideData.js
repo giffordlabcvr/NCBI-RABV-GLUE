b2ff0f7af077d3b59a9bd2dca59d2007f44490c2
Robert Gifford <robjgiff@gmail.com>
2020-12-07 14:57:53 +0000

    Testing script to import updated side-data

--- glue/ncbiRabvImportCuratedSideData.js
+++ glue/ncbiRabvImportCuratedSideData.js
@@ -1,5 +1,12 @@
-//var infiles = [ "Africa3miss.tsv" , "Africa2miss.tsv", "Arctic_miss.tsv" ];
 var infiles = [ "Africa3miss.tsv" ];
+
+//var infiles = [ "Africa3miss.tsv" , "Africa2miss.tsv", 
+//"Arctic_miss.tsv", "Asian_miss.tsv", "Asian_SEA1a_miss.tsv", "Asian_SEA1b_miss.tsv", 
+//"Asian_SEA2a_miss.tsv", "Asian_SEA2b_miss.tsv", "Asian_SEA3_miss.tsv", "Asian_SEA4_miss.tsv", 
+//"CosmoAF1amiss.tsv", "CosmoAF1bmiss.tsv", "CosmoAf1cmiss.tsv", "CosmoAF4miss.tsv", 
+//"CosmoAM_miss.tsv", "CosmoE_miss.tsv", "Cosmomiss.tsv", "Ind_miss.tsv" ];
+
+
 
 // Get the sequence source mappings (i.e. which in ncbi-refseqs, ncbi-curated, etc)
 var epvRefseqResultMap = {};
@@ -29,8 +36,8 @@ for(var i = 0; i < infiles.length; i++) {
 		    //glue.log("INFO", "FIELD: countryEdit:", countryEdit);
 		    //glue.log("INFO", "FIELD: countryUpdate:", countryUpdate);	    
 		    if (countryUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating country field:", isolateObj.country_edit);
-				//glue.command(["set", "field", "m49_country", isolateObj.country_edit]);				
+				glue.log("INFO", "Updating country field to:", isolateObj.country_edit);
+				glue.command(["set", "field", "gb_country", isolateObj.country_edit]);				
 		    }
 		    
         	var placeEdit   = isolateObj.place_edit;
@@ -38,8 +45,8 @@ for(var i = 0; i < infiles.length; i++) {
  		    //glue.log("INFO", "FIELD: placeEdit:", placeEdit);
 		    //glue.log("INFO", "FIELD: placeUpdate:", placeUpdate);
 		    if (placeUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating place field:", isolateObj.placeEdit);
-				//glue.command(["set", "field", "m49_country", isolateObj.placeEdit]);				
+				glue.log("INFO", "Updating place field to:", isolateObj.placeEdit);
+				glue.command(["set", "field", "placeEdit", isolateObj.placeEdit]);				
 		    }
        	
         	var collectionYearEdit   = isolateObj.Collection_year_edit;
@@ -47,8 +54,8 @@ for(var i = 0; i < infiles.length; i++) {
  		    //glue.log("INFO", "FIELD: collectionYearEdit:", collectionYearEdit);
 		    //glue.log("INFO", "FIELD: collectionYearUpdate:", collectionYearUpdate);
 		    if (collectionYearUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating collectionYearEdit field:", isolateObj.collectionYearEdit);
-				//glue.command(["set", "field", "collection_year", isolateObj.collectionYearEdit]);				
+				glue.log("INFO", "Updating collection_year field to:", isolateObj.collectionYearEdit);
+				glue.command(["set", "field", "collection_year", isolateObj.collectionYearEdit]);				
 		    }
        	
         	var earliestCollectionYearEdit   = isolateObj.earliest_collection_edit;
@@ -56,8 +63,8 @@ for(var i = 0; i < infiles.length; i++) {
  		    //glue.log("INFO", "FIELD: earliestCollectionYearEdit:", earliestCollectionYearEdit);
 		    //glue.log("INFO", "FIELD: earliestCollectionYearUpdate:", earliestCollectionYearUpdate);
 		    if (earliestCollectionYearUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating earliest_collection_year field:", isolateObj.earliestCollectionYearEdit);
-				//glue.command(["set", "field", "earliest_collection_year", isolateObj.earliestCollectionYearEdit]);				
+				glue.log("INFO", "Updating earliest_collection_year field to:", isolateObj.earliestCollectionYearEdit);
+				glue.command(["set", "field", "earliest_collection_year", isolateObj.earliestCollectionYearEdit]);				
 		    }
 
         	var latestCollectionYearEdit     = isolateObj.latest_collection_edit;
@@ -66,7 +73,7 @@ for(var i = 0; i < infiles.length; i++) {
 		    //glue.log("INFO", "FIELD: latestCollectionYearUpdate:", latestCollectionYearUpdate);
 		    if (latestCollectionYearUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating latestCollectionYearEdit field:", isolateObj.latestCollectionYearEdit);
-				//glue.command(["set", "field", "latest_collection_year", isolateObj.latestCollectionYearEdit]);				
+				glue.command(["set", "field", "latest_collection_year", isolateObj.latestCollectionYearEdit]);				
 		    }
 
         	var hostEdit       = isolateObj.host_edit;
@@ -78,7 +85,7 @@ for(var i = 0; i < infiles.length; i++) {
 		    if (hostUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating host field:", isolateObj.hostEdit);
 				glue.log("INFO", "Updating host common name field:", isolateObj.hostCommonEdit);
-				//glue.command(["set", "field", "host", isolateObj.hostEdit]);				
+				glue.command(["set", "field", "host", isolateObj.hostEdit]);				
 		    }
 
         	var labConstruct   = isolateObj.Lab_construct;
@@ -94,7 +101,7 @@ for(var i = 0; i < infiles.length; i++) {
 		    //glue.log("INFO", "FIELD: pubmedUpdate:", pubmedUpdate);
 		    if (pubmedUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating gb_pubmed_id field:", isolateObj.pubmedEdit);
-				//glue.command(["set", "field", "host", isolateObj.hostEdit]);				
+				glue.command(["set", "field", "host", isolateObj.hostEdit]);				
 		    }
 
 		});
@@ -112,7 +119,7 @@ function get_sequence_source_mappings(epvRefseqResultMap) {
 		
 	_.each(resultList,function(resultObj){
 
-		glue.log("INFO", "This result is:", resultObj);
+		//glue.log("INFO", "This result is:", resultObj);
 		var sequenceID = resultObj["sequenceID"];
 		var sourceName = resultObj["source.name"];
 		//var sourceName = ["ncbi-refseqs"];
