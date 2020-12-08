f8ce54d6be4c8fc8591d78a956532e7f86256903
Robert Gifford <robjgiff@gmail.com>
2020-12-08 12:47:46 +0000

    Creating upload script for curated (fixed) data

--- glue/ncbiRabvImportCuratedSideData.js
+++ glue/ncbiRabvImportCuratedSideData.js
@@ -21,66 +21,66 @@ for(var i = 0; i < infiles.length; i++) {
 	var loadResult;
 	glue.inMode("module/tabularUtilityTab", function() {
 		loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/"+infile]));
-		//glue.log("INFO", "load result was:", loadResult);
+		glue.log("INFO", "load result was:", loadResult);
 	});
 
 	_.each(loadResult, function(isolateObj) {
 
-        var sequenceID = isolateObj.sequence_sequenceID;
+        var sequenceID = isolateObj.sequenceID;
         var sourceName = epvRefseqResultMap[sequenceID];
-		glue.log("INFO", "Entering sequence table data for isolate:", sequenceID, " from source '", sourceName, "'");
+		glue.log("INFO", "### Entering sequence table data for isolate:", sequenceID, " from source '", sourceName, "'");
 		glue.inMode("sequence/"+sourceName+"/"+sequenceID, function() {
 
+        	var countryUpdate = isolateObj.country_update;
         	var countryEdit   = isolateObj.country_edit;
-        	var countryUpdate = isolateObj.Country_update;
-		    //glue.log("INFO", "FIELD: countryEdit:", countryEdit);
-		    //glue.log("INFO", "FIELD: countryUpdate:", countryUpdate);	    
+		    glue.log("INFO", "FIELD: countryEdit:", countryEdit);
+		    glue.log("INFO", "FIELD: countryUpdate:", countryUpdate);	    
 		    if (countryUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating country field to:", isolateObj.country_edit);
-				glue.command(["set", "field", "gb_country", isolateObj.country_edit]);				
+				glue.log("INFO", "Updating country field to:", countryEdit);
+				glue.command(["set", "field", "gb_country", countryUpdate]);				
 		    }
 		    
         	var placeEdit   = isolateObj.place_edit;
         	var placeUpdate = isolateObj.place_update;
- 		    //glue.log("INFO", "FIELD: placeEdit:", placeEdit);
-		    //glue.log("INFO", "FIELD: placeUpdate:", placeUpdate);
+ 		    glue.log("INFO", "FIELD: placeEdit:", placeEdit);
+		    glue.log("INFO", "FIELD: placeUpdate:", placeUpdate);
 		    if (placeUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating place field to:", isolateObj.placeEdit);
-				glue.command(["set", "field", "placeEdit", isolateObj.placeEdit]);				
+				glue.log("INFO", "Updating place field to:", placeEdit);
+				glue.command(["set", "field", "placeEdit", placeEdit]);				
 		    }
        	
-        	var collectionYearEdit   = isolateObj.Collection_year_edit;
-        	var collectionYearUpdate = isolateObj.Collection_year_update;
- 		    //glue.log("INFO", "FIELD: collectionYearEdit:", collectionYearEdit);
-		    //glue.log("INFO", "FIELD: collectionYearUpdate:", collectionYearUpdate);
+        	var collectionYearEdit   = isolateObj.collection_year_edit;
+        	var collectionYearUpdate = isolateObj.collection_year_update;
+ 		    glue.log("INFO", "FIELD: collectionYearEdit:", collectionYearEdit);
+		    glue.log("INFO", "FIELD: collectionYearUpdate:", collectionYearUpdate);
 		    if (collectionYearUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating collection_year field to:", isolateObj.collectionYearEdit);
-				glue.command(["set", "field", "collection_year", isolateObj.collectionYearEdit]);				
+				glue.log("INFO", "Updating collection_year field to:", collectionYearEdit);
+				glue.command(["set", "field", "collection_year", collectionYearEdit]);				
 		    }
        	
         	var earliestCollectionYearEdit   = isolateObj.earliest_collection_edit;
         	var earliestCollectionYearUpdate = isolateObj.earliest_collection_update;
- 		    //glue.log("INFO", "FIELD: earliestCollectionYearEdit:", earliestCollectionYearEdit);
-		    //glue.log("INFO", "FIELD: earliestCollectionYearUpdate:", earliestCollectionYearUpdate);
+ 		    glue.log("INFO", "FIELD: earliestCollectionYearEdit:", earliestCollectionYearEdit);
+		    glue.log("INFO", "FIELD: earliestCollectionYearUpdate:", earliestCollectionYearUpdate);
 		    if (earliestCollectionYearUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating earliest_collection_year field to:", isolateObj.earliestCollectionYearEdit);
-				glue.command(["set", "field", "earliest_collection_year", isolateObj.earliestCollectionYearEdit]);				
+				glue.log("INFO", "Updating earliest_collection_year field to:", earliestCollectionYearEdit);
+				glue.command(["set", "field", "earliest_collection_year", earliestCollectionYearEdit]);				
 		    }
 
         	var latestCollectionYearEdit     = isolateObj.latest_collection_edit;
         	var latestCollectionYearUpdate   = isolateObj.latest_collection_update;
- 		    //glue.log("INFO", "FIELD: latestCollectionYearEdit:", latestCollectionYearEdit);
-		    //glue.log("INFO", "FIELD: latestCollectionYearUpdate:", latestCollectionYearUpdate);
+ 		    glue.log("INFO", "FIELD: latestCollectionYearEdit:", latestCollectionYearEdit);
+		    glue.log("INFO", "FIELD: latestCollectionYearUpdate:", latestCollectionYearUpdate);
 		    if (latestCollectionYearUpdate != 'unchanged') {		    	
-				glue.log("INFO", "Updating latestCollectionYearEdit field:", isolateObj.latestCollectionYearEdit);
-				glue.command(["set", "field", "latest_collection_year", isolateObj.latestCollectionYearEdit]);				
+				glue.log("INFO", "Updating latestCollectionYearEdit field:", latestCollectionYearEdit);
+				glue.command(["set", "field", "latest_collection_year", latestCollectionYearEdit]);				
 		    }
 
         	var hostEdit       = isolateObj.host_edit;
         	var hostUpdate     = isolateObj.host_update;
         	var hostCommonEdit = isolateObj.host_common_edit;
- 		    //glue.log("INFO", "FIELD: hostEdit:", hostEdit);
-		    //glue.log("INFO", "FIELD: hostUpdate:", hostUpdate);
+ 		    glue.log("INFO", "FIELD: hostEdit:", hostEdit);
+		    glue.log("INFO", "FIELD: hostUpdate:", hostUpdate);
 		    glue.log("INFO", "FIELD: hostCommonEdit:", hostCommonEdit);
 		    if (hostUpdate != 'unchanged') {		    	
 				glue.log("INFO", "Updating host field:", isolateObj.hostEdit);
